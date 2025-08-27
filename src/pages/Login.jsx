b634cff779752d8884123cda413a9c5303292662
Joon <joonhyoo12@gmail.com>
2025-08-28 00:40:59 +1000

    changed redirectTo /home

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -18,7 +18,7 @@ export default function Login() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'github',
       options: {
-        redirectTo: window.location.href,
+        redirectTo: '/home',
       },
     });
 
